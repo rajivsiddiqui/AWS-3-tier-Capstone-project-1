13201ea076bdf76f5892201b46b19b3447e62abc
rajivsiddiqui <rajivsiddiqui@gmail.com>
2026-07-10 20:01:55 +0600

    fix:database username and password change in jenkinsfile and terraform file

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -226,7 +226,7 @@ resource "aws_db_instance" "mysql" {
   instance_class         = "db.t3.micro"
   allocated_storage      = 20
   db_name                = "capstone"
-  username               = "admin"
+  username               = "appuser"
   password               = var.db_password
   db_subnet_group_name   = aws_db_subnet_group.main.name
   vpc_security_group_ids = [aws_security_group.rds.id]
